609a3932b7e1166b48571650ee5c51fc8b5130e6
MK <michal@toke.com>
2015-12-12 16:50:20 +0100

    sprawdzanie polaczenia dziala

--- site_connectinfo/Program.cs
+++ site_connectinfo/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
 
 namespace site_connectinfo
 {
@@ -10,6 +12,32 @@ namespace site_connectinfo
     {
         static void Main(string[] args)
         {
+
+            string adresStrony = "www.spoko.poznan.pl";
+            var adresy = Dns.GetHostAddresses(adresStrony);
+            var port = 80;
+            foreach (var adresIp in adresy)
+            {
+                var gniazdo = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                try
+                {
+                    gniazdo.Connect(adresIp, port);
+                    Console.WriteLine("Strona: " + adresStrony);
+                    Console.WriteLine("IP serwera: " + ((IPEndPoint)gniazdo.RemoteEndPoint).Address.ToString());
+                    Console.WriteLine("Numer portu zdalnego połączenia: " + ((IPEndPoint)gniazdo.RemoteEndPoint).Port.ToString());
+                    Console.WriteLine("Numer portu lokalnego z którego dokonywane jest połączenie " + ((IPEndPoint)gniazdo.LocalEndPoint).Port.ToString());
+                }
+                catch (SocketException)
+                {
+                    Console.WriteLine("błąd połączenia z portem");
+                }
+                finally
+                {
+                    gniazdo.Dispose();
+                }
+            }
+
+            Console.ReadKey();
         }
     }
 }
